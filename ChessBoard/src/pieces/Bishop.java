954251e21fb03c5d79e9fd8f644422bd91e7e5bf
Thevindu Sithujaya <thevindusithujaya@gmail.com>
2026-04-14 15:49:49 +0530

    Initialize Bishop with constructor and chessboard placement

--- ChessBoard/src/pieces/Bishop.java
+++ ChessBoard/src/pieces/Bishop.java
@@ -1,6 +1,23 @@
 package pieces;
 
-public class Bishop extends Piece{
+import board.ChessBoard;
+
+public class Bishop extends Piece {
+
+    private boolean check = false;
+
+    public Bishop(Character col, int row, boolean white) {
+        setCol(col);
+        setRow(row);
+
+        if (white) {
+            setIdentification(PieceIdentification.W_BISHOP);
+        } else {
+            setIdentification(PieceIdentification.B_BISHOP);
+        }
+        ChessBoard.insertPiece(row, turnColToIndex(col), this);
+    }
+
     @Override
     public void moveCheck() {
 
