5e6dfc55b47c0859e3cb028e3fa53f5f308e558b
Thevindu Sithujaya <thevindusithujaya@gmail.com>
2026-04-16 13:08:25 +0530

    feat: add Queen class constructor and basic structure

--- ChessBoard/src/pieces/Queen.java
+++ ChessBoard/src/pieces/Queen.java
@@ -1,13 +1,29 @@
 package pieces;
 
-public class Queen extends Piece{
+import Global.Global;
+import board.ChessBoard;
+
+public class Queen extends Piece {
+
+    private boolean check = false;
+
+    public Queen(Character chessCol, int chessRow, boolean white) {
+        setChessCol(chessCol);
+        setChessRow(chessRow);
+
+        if (white) {
+            setIdentification(PieceIdentification.W_QUEEN);
+        } else {
+            setIdentification(PieceIdentification.B_QUEEN);
+        }
+        ChessBoard.insertPiece(chessCol, chessRow, this);
+    }
+
     @Override
     public void moveCheck() {
-
     }
 
     @Override
     public void movePiece() {
-
     }
 }
